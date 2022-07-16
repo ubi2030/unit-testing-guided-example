1f08f8bc0c04982095e3effc2dd1266a48ec23b8
Jonathan Herzog <jonathan@Jonathans-MacBook-Pro-3.local>
2022-07-16 12:05:24 -0400

    add sum, subtract, divide functions

--- calculator.js
+++ calculator.js
@@ -1,13 +1,28 @@
 function sum(a, b) {
-  return;
-}
+  if(a === undefined){
+    a=0;
+  }
+  if(b === undefined){
+    b=0;
+  }
+  return a+b;
+  }
 
 function subtract(a, b) {
-  return;
+  if(a === undefined){
+    a=0;
+  }
+  if(b === undefined){
+    b=0;
+  }
+  return a-b;
 }
 
 function divide(a, b) {
-  return;
+  if(b === 0){
+    throw new Error('Cannot divide by 0');
+  }
+  return a/b;
 }
 
 function multiply(a, b) {
